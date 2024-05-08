dcfdf33410bf4eae80a2427730eb5d7de28e2897
navratil-matej <matej.navratil@tul.cz>
2024-05-08 13:20:26 +0200

    Add template semestral project

---
+++ ppj-2024/src/main/java/net/cuddlebat/ppj2024/Ppj2024Application.java
@@ -0,0 +1,13 @@
+package net.cuddlebat.ppj2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ppj2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ppj2024Application.class, args);
+	}
+
+}
---
+++ ppj-2024/src/test/java/net/cuddlebat/ppj2024/Ppj2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cuddlebat.ppj2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ppj2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
